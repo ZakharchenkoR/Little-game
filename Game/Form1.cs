aa9d74f249edb5aea069680debc050f505d0498b
Lenovo <Lenovo@DESKTOP-43JFBJL>
2018-10-17 22:47:18 +0300

    Добавьте файлы проекта.

---
+++ Game/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Game : Form
+    {
+        Game_g game;
+        public Game()
+        {
+            InitializeComponent();
+            game = new Game_g(4);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt32(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.chec_numbers())
+            {
+                MessageBox.Show("You are victory!");
+                Start_Game();
+            }
+        }
+
+        private Button button (int position)
+        {
+            switch(position)
+            {
+                case 0: return button0;
+                case 1: return button1;
+                case 2: return button2;
+                case 3: return button3;
+                case 4: return button4;
+                case 5: return button5;
+                case 6: return button6;
+                case 7: return button7;
+                case 8: return button8;
+                case 9: return button9;
+                case 10: return button10;
+                case 11: return button11;
+                case 12: return button12;
+                case 13: return button13;
+                case 14: return button14;
+                case 15: return button15;
+                default:return null;
+            }
+        }
+
+        private void Start_Game()
+        {
+            game.start();
+            for (int i = 0; i < 100; i++)
+                game.shift_random();
+            refresh();
+        }
+
+        
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+            Start_Game();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            Start_Game();
+        }
+
+        private void refresh()
+        {
+            for (int position = 0; position < 16; position++)
+            {
+                int num = game.get_numder(position);
+                button(position).Text = num.ToString();
+                button(position).Visible = (num > 0);
+
+            }
+        }
+    }
+}
---
+++ Game/Game.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Game_g
+    {
+        int size;
+        int[,] map;
+        int spase_x;
+        int spase_y;
+        public static Random rand = new Random();
+        public Game_g(int size)
+        {
+            if (size < 2)
+                size = 2;
+            if (size > 5)
+                size = 5;
+            this.size = size;
+            map = new int[size, size];
+        } 
+
+        public void start()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    map[i, y] = coor_to_position(i, y) + 1;
+                }
+            }
+            spase_x = size - 1;
+            spase_y = size - 1;
+            map[spase_x,spase_y] = 0;
+            
+        }
+
+        public int get_numder(int position)
+        {
+            int x;
+            int y;
+            position_to_coor(position, out x, out y);
+            if (x < 0 || x > size)
+                return 0;
+            if (y < 0 || y > size)
+                return 0;
+            return map[x, y];
+        }
+
+
+        public void shift_random()
+        {
+            int a = rand.Next(0, 4);
+            int x = spase_x;
+            int y = spase_y;
+            switch(a)
+            {
+                case 0:x--;break;
+                case 1:x++;break;
+                case 2: y--;break;
+                case 3: y++;break;
+            }
+            shift(coor_to_position(x, y));
+        }
+
+
+        public void shift(int position)
+        {
+            int x;
+            int y;
+            position_to_coor(position, out x, out y);
+            if (Math.Abs(spase_x - x) + Math.Abs(spase_y - y) != 1)
+                return;
+            map[spase_x, spase_y] = map[x, y];
+            map[x, y] = 0;
+            spase_x = x;
+            spase_y = y;
+        }
+
+        private int coor_to_position(int x,int y)
+        {
+            if (x < 0)
+                x = 0;
+            if (x > size - 1)
+                x = size - 1;
+            if (y < 0)
+                y = 0;
+            if (y > size - 1)
+                y = size - 1;     
+            return y * size + x;
+        }
+
+        private void position_to_coor(int position, out int x,out int y)
+        {
+            if (position < 0)
+                position = 0;
+            if (position > size * size - 1)
+                position = size * size - 1;
+            x = position % size;
+            y = position / size;
+        }
+
+        public bool chec_numbers()
+        {
+            if (!(spase_x == size - 1 &&
+                spase_y == size - 1))
+                return false;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (!(i == size - 1 && spase_y == size - 1))
+                       if(map[i,j] != coor_to_position(i,j)+1)
+                         return false;
+                }
+            }
+            return true;
+                  
+        }
+
+
+
+    }
+}
